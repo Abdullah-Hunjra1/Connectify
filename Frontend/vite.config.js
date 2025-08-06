c4f34910455f36fb8224614ee13378d4d484bc15
Abdullah-Hunjra1 <abdullahhunjra111@gmail.com>
2025-08-06 19:51:51 +0500

    update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5174, // ✅ Set this if you want Vite to always run on 5174
-  },
 })
