b50cbcc6c128c4262e2f9c049172ce091d4af1cc
Abdullah Jutt <abdullahhunjra111@gmail.com>
2026-07-18 19:12:56 +0500

    Add root route to confirm backend is running

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -37,6 +37,10 @@ app.use("/api/chat-page", chatRoute);
 //   });
 // }
 
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
