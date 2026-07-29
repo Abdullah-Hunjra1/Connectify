1c62ca8c8a2757409ac21bfc52348e22792032cd
Abdullah Jutt <abdullahhunjra111@gmail.com>
2026-07-29 14:06:55 +0500

    Refactor axios configuration for API URL

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,21 @@
+// import axios from "axios";
+
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+// export const axiosInstance = axios.create({
+//     baseURL: BASE_URL,
+//     withCredentials: true,   // This allows cookies to be sent with requests
+// })
+
+
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true,   // This allows cookies to be sent with requests
-})
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
