2768b52a6f69269b8bcf3fcefcf10fbc6a315a1c
Abdullah Jutt <abdullahhunjra111@gmail.com>
2026-07-29 14:15:44 +0500

    Update API base URL for production environment

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -13,7 +13,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : import.meta.env.VITE_API_URL;
+    : "https://connectify-server-one.vercel.app/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
